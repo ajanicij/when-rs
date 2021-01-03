c7f8ade60f090f41c83733ba9350bb85db8ccd72
Aleksandar Janicijevic <ajanicij@yahoo.com>
2021-01-02 23:44:45 -0500

    Implemented initialization.

--- src/main.rs
+++ src/main.rs
@@ -3,54 +3,18 @@ use std::process;
 use std::env;
 use std::path;
 use std::path::Path;
-use std::fs::File;
-use std::io::BufReader;
-use std::io::BufRead;
+use std::fs::{self, File};
+use std::io::{self, BufReader, BufRead, Write};
 use chrono::{Local, Duration};
+use std::fmt::{Display, Debug};
+use atty::Stream;
 
 mod preferences;
 mod date;
 mod datecalc;
 mod utils;
 
-fn main() {
-    let home_dir: String;
-    match env::var("HOME") {
-        Ok(home) => home_dir = String::from(home),
-        Err(_) => {
-            eprintln!("HOME directory unknown");
-            process::exit(0);
-        }
-    }
-
-    // println!("HOME: {}", home_dir);
-    let preferences_path = path::Path::new(&home_dir).join(".when-rs").join("preferences")
-        .to_str()
-        .unwrap()
-        .to_string();
-    println!("preferences: {}", preferences_path);
-
-    let it = std::fs::read_to_string(&preferences_path);
-    if let Err(err) = it {
-        eprintln!("Couldn't open {}: {}", preferences_path, err);
-        process::exit(-1);
-    }
-    let it = it.unwrap();
-
-    // Read preferences from preferences file.
-    let hashmap_preferences = preferences::parse_lines(it.lines());
-
-    // eprintln!("preferences: {:?}", hashmap_preferences);
-    let _calendar;
-    match hashmap_preferences.get("calendar") {
-        Some(p) => _calendar = Path::new(p),
-        None => {
-            eprintln!("Configuration doesn't have calendar key");
-            process::exit(-1);
-        }
-    }
-
-    // Parse command line arguments.
+fn get_arg() -> clap::ArgMatches<'static> {
     let matches = App::new("when-rs")
         .version("0.1")
         .about("Simple personal calendar utility")
@@ -81,28 +45,169 @@ set up the first time you run when-rs."#)
         )
         .get_matches();
 
+    matches
+}
+
+fn expect<Data, Error>(r: Result<Data, Error>, message: &str) -> Data
+    where Error: Debug + Display
+{
+    if let Err(err) = r {
+        eprintln!("{}: {}", message, err);
+        process::exit(-1);
+    }
+    r.unwrap()
+}
+
+fn expect_option<Data>(r: Option<Data>, message: &str) -> Data
+{
+    if r.is_none() {
+        eprintln!("{}", message);
+        process::exit(-1);
+    }
+    r.unwrap()
+}
+
+fn prompt(message: &str) -> Option<String> {
+    println!("{}", message);
+    let mut buffer = String::new();
+    let stdin = io::stdin();
+    match stdin.read_line(&mut buffer) {
+        Ok(_) => Some(buffer.trim_end().to_string()),
+        _ => None
+    }
+}
+
+// home_subdir creates path string of a directory that is under current
+// user's home directory, e.g.
+// home_subdir(vec![".when".to_string(), "preferences".to_string()]) returns
+// string "/home/username/.when/preferences".
+fn home_subdir(l: Vec<String>) -> String {
+    let home_dir: String;
+
+    home_dir = expect(env::var("HOME"), "HOME unknown");
+
+    let path = path::Path::new(&home_dir);
+    let mut path_buf = path.to_path_buf();
+    for s in l {
+        path_buf = path_buf.join(s);
+    }
+    let path = path_buf
+                   .to_str()
+                   .unwrap()
+                   .to_string();
+    return path;
+}
+
+fn initialize(_preferences: &str) {
+    if !atty::is(Stream::Stdout) || !atty::is(Stream::Stdin) {
+        eprintln!("Not in interactive mode!");
+        process::exit(-1);
+    }
+
+    // println!("Initializing");
+    let a1 = prompt(r#"
+You can now set up your calendar. This involves creating a directory ~/.when, and making
+a couple of files in it. If you want to do this, type y and hit return."#).unwrap();
+    println!("a1 is {}", a1);
+    if a1 != "y" {
+        process::exit(0);
+    }
+    // println!("You said yes");
+    let mut editor = prompt(r#"
+You can edit your calendar file using your favorite editor. Please enter the command you
+want to use to run your editor, or hit return to accept this default:
+  emacs -nw"#).unwrap();
+     if editor == "" {
+         editor = "emacs -nw".to_string();
+     }
+
+    let when_rs_path = home_subdir(vec![
+        ".when-rs".to_string()
+    ]);
+    let preferences_path = home_subdir(vec![
+        ".when-rs".to_string(),
+        "preferences".to_string()
+    ]);
+    let calendar_path = home_subdir(vec![
+        ".when-rs".to_string(),
+        "calendar".to_string()
+    ]);
+    println!("preferences_path is {}", preferences_path);
+    println!("editor is {}", editor);
+    if let Err(error) = fs::create_dir(&when_rs_path) {
+        eprintln!("Error creating directory {}: {}", when_rs_path, error);
+        process::exit(-1);
+    }
+    let file = File::create(&preferences_path);
+    if let Err(error) = file {
+        eprintln!("Error creating file {}: {}", preferences_path, error);
+        process::exit(-1);
+    }
+    let mut file = file.unwrap();
+    expect(writeln!(&mut file, "calendar = {}", calendar_path),
+        &format!("Writing to file {}", preferences_path));
+    expect(writeln!(&mut file, "editor = {}", editor),
+        &format!("Writing to file {}", preferences_path));
+
+    let mut calendar_file = File::create(&calendar_path);
+    if let Err(error) = calendar_file {
+        eprintln!("@#$% Error creating file {}: {}", calendar_path, error);
+        process::exit(-1);
+    }
+    let mut file = calendar_file.unwrap();
+
+    // process::exit(0);
+    println!(r#"
+You can now add items to your calendar file. Do ``when-rs --help'' for more information.
+    "#);
+}
+
+fn main() {
+    let home_dir: String;
+
+    home_dir = expect(env::var("HOME"), "HOME unknown");
+
+//    let preferences_path = path::Path::new(&home_dir).join(".when-rs").join("preferences")
+//        .to_str()
+//        .unwrap()
+//        .to_string();
+    // println!("preferences: {}", preferences_path);
+
+    let preferences_path = home_subdir(vec![
+        ".when-rs".to_string(),
+        "preferences".to_string()
+    ]);
+    println!("preferences_path is {}", preferences_path);
+
+    let it = std::fs::read_to_string(&preferences_path);
+    let preferences: String;
+    if it.is_err() {
+        initialize(&preferences_path);
+        preferences = std::fs::read_to_string(&preferences_path).unwrap();
+        
+    } else {
+        preferences = it.unwrap();
+    }
+
+
+    // Read preferences from preferences file.
+    let hashmap_preferences = preferences::parse_lines(preferences.lines());
+
+    let calendar = expect_option(
+        hashmap_preferences.get("calendar"), "Configuration doesn't define calendar");
+
+    // Parse command line arguments.
+    let matches = get_arg();
+
     let mut arg_future: i32 = 14;
     let mut arg_past: i32 = -1;
 
     if let Some(n) = matches.value_of("future") {
-        match n.parse::<i32>() {
-            Ok(future) => arg_future = future,
-            _ => {
-                eprintln!("{}", matches.usage());
-                process::exit(-1);
-            }
-        }
-        // println!("future is {}", n);
+        arg_future = expect(n.parse::<i32>(), matches.usage());
     }
 
     if let Some(n) = matches.value_of("past") {
-        match n.parse::<i32>() {
-            Ok(past) => arg_past = past,
-            _ => {
-                eprintln!("{}", matches.usage());
-                process::exit(-1);
-            }
-        }
+        arg_past = expect(n.parse::<i32>(), matches.usage());
     }
 
     // Get calendar from calendar file, specified in command line or
